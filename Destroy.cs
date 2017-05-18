562e7a107c13d1491831e1c692a156014ed13a93
OOOliver <834543912@qq.com>
2017-05-18 10:38:28 +0800

    Add files via upload

--- Destroy.cs
+++ Destroy.cs
@@ -4,28 +4,11 @@ using UnityEngine;
 
 
 public class Destroy : MonoBehaviour{
-	public float x;
-	public float y;
-	void DestroyByCamera(){
-		if (transform.position.x > x || transform.position.x < -x) {
-			Destroy (gameObject);
-		}
-		if (transform.position.y > y || transform.position.y < -y) {
-			Destroy (gameObject);
-		}
-	}
-	void OnCollisionEnter2D(Collision2D other){
-		if (other.gameObject.tag == "hball") {
-			Destroy (other.gameObject);
-			Destroy (gameObject);
-		}
+	void OnTriggerEnter2D(Collider2D other){
 		if (other.gameObject.tag == "eball") {
 			Destroy (gameObject);
 			Destroy (other.gameObject);
 		}
-	}
-	void Update(){
-		DestroyByCamera ();
 	}
 }
 
---
+++ Ep.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Ep : MonoBehaviour{
+	void Start(){
+		Destroy (gameObject, 3);
+	}
+
+	void FixedUpdate(){
+		transform.Rotate (new Vector3 (0, 0, 90) * Time.deltaTime);
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+		if (other.gameObject.tag == "hball" || other.gameObject.tag == "eball") {
+			Destroy (other.gameObject);
+		}
+	}
+}
+
